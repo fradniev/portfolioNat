ba6f67f1ce4ff123402b4ef7222cd8ae33bbef91
Fradniev <fradniev@hotmail.com>
2021-11-12 23:36:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const path = require('path')
+var app = express();
+
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+/*LINKS DE PAGINAS*/
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/" + "index.html");
+ })
+ app.get('/cv', function (req, res) {
+    res.sendFile(__dirname + "/public/html/" + "listuser.html");
+ })
+
+ /*INICIAR SERVIDOR*/
+ var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
