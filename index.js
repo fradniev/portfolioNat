d444fcb820fa7f63b8408927bc457a409705d73d
Fradniev <fradniev@hotmail.com>
2021-11-19 23:08:03 -0300

    changed port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
  })
 
  /*INICIAR SERVIDOR*/
- var server = app.listen(8081, function () {
+ var server = app.listen(80, function () {
     var host = server.address().address
     var port = server.address().port
     
