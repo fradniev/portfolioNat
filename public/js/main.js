774c0e318881efc27fcfd42c363527141ce22e5d
Fradniev <fradniev@hotmail.com>
2021-12-03 17:40:43 -0300

    cv and project updated

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,20 @@ $( document ).ready(function() {
     $( ".hover-proyecto" ).mouseover(function() {
       $( ".proyecto-seleccionado img" ).attr("src","/images/proyect/"+$(this).data("image"));
     });
+    $(".pagination li").click(function(){
+      $(".project-active").removeClass("project-active")
+      if($(this).data("page")=="2"){
+        $(".project-container-1").addClass("go-left"); 
+        $(".project-container-2").removeClass("go-right"); 
+      } else {
+        $(".project-container-2").addClass("go-right"); 
+        $(".project-container-1").removeClass("go-left"); 
+      }
+      $(".project-container-"+$(this).data("page")).addClass("project-active")
+      $(".bi-record-fill").removeClass("bi-record-fill").addClass("bi-record")
+      $(this).find(".bi").removeClass("bi-record").addClass("bi-record-fill")
+    })
+    $(".main-section").click(function(){
+      $(this).find(".toHide").toggleClass("hidden-section")
+    })
 });
